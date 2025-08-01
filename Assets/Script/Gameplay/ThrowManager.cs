dbba4544d5e867bcb5bfbcb5148fbd27d63c7a38
KanpitchaMacbook <ksinitwarakun@gmail.com>
2025-08-01 21:23:31 +0700

    [Add] throw on human with charge power bar and projectile

--- Assets/Script/Gameplay/ThrowManager.cs
+++ Assets/Script/Gameplay/ThrowManager.cs
@@ -4,34 +4,56 @@ public class ThrowManager : MonoBehaviour
 {
     [SerializeField] private Transform _humanHandSpawnPoint;
     [SerializeField] private GameObject[] _humanItemPrefabs;
-    [SerializeField] private float _throwPower = 10f;
+    [SerializeField] private float _minThrowPower = 4f;
+    [SerializeField] private float _maxThrowPower = 8f;
+    [SerializeField] private float _chargeSpeed = 18f; // หน่วย/วินาที
+    [SerializeField] private float _throwAngleY = 6f;
     [SerializeField] private bool _isHuman;
+    [SerializeField] private PowerBarUI _powerBarUI;
+    private float _currentPower;
+    private bool _isCharging;
 
-    private void Update()
+    void Update()
     {
-        // test on pc
         if (Input.GetMouseButtonDown(0))
         {
-            ThrowItem();
+            _isCharging = true;
+            _currentPower = _minThrowPower;
+            _powerBarUI.ShowHumanPowerBar(true);
+        }
+
+        if (_isCharging)
+        {
+            _currentPower += _chargeSpeed * Time.deltaTime;
+            _currentPower = Mathf.Clamp(_currentPower, _minThrowPower, _maxThrowPower);
+
+            // update UI
+            float t = (_currentPower - _minThrowPower) / (_maxThrowPower - _minThrowPower);
+            _powerBarUI.SetHumanPower(t); // chargePercent = 0-1
+        }
+
+        if (Input.GetMouseButtonUp(0) && _isCharging)
+        {
+            _isCharging = false;
+            _powerBarUI.ShowHumanPowerBar(false);
+            ThrowItem(_currentPower);
         }
     }
 
-    public void ThrowItem()
+    public void ThrowItem(float force)
     {
         // select prefab from random
         GameObject prefab = _humanItemPrefabs[Random.Range(0, _humanItemPrefabs.Length)];
-        // create spawn point
         GameObject item = Instantiate(prefab, _humanHandSpawnPoint.position, Quaternion.identity);
+
         Rigidbody rb = item.GetComponent<Rigidbody>();
         if (rb != null)
         {
-            // shoot them out
-            Vector3 throwDir = _isHuman ? Vector3.left : Vector3.right;
+            Vector3 throwDir = (_isHuman ? Vector3.left : Vector3.right) * force
+                 + Vector3.up * _throwAngleY;
+            rb.AddForce(throwDir, ForceMode.Impulse);
 
-            rb.AddForce(throwDir * _throwPower, ForceMode.Impulse);
-
-            // Optional: Debug direction
-            Debug.DrawRay(_humanHandSpawnPoint.position, throwDir * 2, Color.red, 1f);
+            Debug.DrawRay(_humanHandSpawnPoint.position, throwDir.normalized * 2, Color.red, 1f);
         }
     }
 }
--- Assets/Script/UI/PowerBarUI.cs
+++ Assets/Script/UI/PowerBarUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerBarUI : MonoBehaviour
 {
@@ -7,10 +8,23 @@ public class PowerBarUI : MonoBehaviour
     [SerializeField] private GameObject _humanTimeWarning;
     [SerializeField] private GameObject _zombieTimeWarning;
 
+    // fill
+    [SerializeField] private Image _humanPowerBarImage;
+    [SerializeField] private Image _zombiePowerBarImage;
+
     public void ShowHumanPowerBar(bool show) => _humanPowerBar?.SetActive(show);
     public void ShowZombiePowerBar(bool show) => _zombiePowerBar?.SetActive(show);
 
     public void ShowHumanTimeWarning(bool show) => _humanTimeWarning?.SetActive(show);
     public void ShowZombieTimeWarning(bool show) => _zombieTimeWarning?.SetActive(show);
+
+    public void SetHumanPower(float t)
+    {
+        if (_humanPowerBarImage) _humanPowerBarImage.fillAmount = t;
+    }
+    public void SetZombiePower(float t)
+    {
+        if (_zombiePowerBarImage) _zombiePowerBarImage.fillAmount = t;
+    }
 
 }
